e51b90a7046ca5d1eb093cff9cc3436401a435bd
Ciel <imwithye@gmail.com>
2014-02-24 22:34:07 +0800

    Add default value for Connector.

--- socket/src/main/java/studymaster/socket/Connector.java
+++ socket/src/main/java/studymaster/socket/Connector.java
@@ -8,11 +8,28 @@ import org.java_websocket.handshake.ServerHandshake;
 import java.nio.channels.NotYetConnectedException;
 
 public final class Connector extends WebSocketClient{
-    private static String localServer;
-    private static Connector instance;
-    private static Callback localDelegate;
-    private static String localSender;
-    private static String localClient;
+    private static class defaultDelegate implements Callback {
+        public void onOpen(short httpStatus, String httpStatusMessage) {
+            System.out.println("[debug] (Connector.defaultDelegate onOpen): Connection open, using default delegate.");
+        }
+
+        public void onClose(int code, String reason, boolean remote) {
+            System.out.println("[debug] (Connector.defaultDelegate onClose): Connection close, using default delegate.");
+        }
+
+        public void onMessage(String message) {
+            System.out.println("[debug] (Connector.defaultDelegate onMessage): Receiving message, using default delegate.");
+        }
+
+        public void onError(Exception ex) {
+            System.err.println("[error] (Connector.defaultDelegate onError): An error, using default delegate.");
+        }
+    }
+    private static String localServer = "ws://localhost";
+    private static Connector instance = null;
+    private static Callback localDelegate = null;
+    private static String localSender = "Default Sender";
+    private static String localEndpoint = "Default Connector";
 
     private Connector(URI serverURI) {
         super(serverURI);
@@ -20,8 +37,8 @@ public final class Connector extends WebSocketClient{
 
     public static Connector getInstance() {
         if(instance==null){
-            if(localServer==null && localDelegate==null && localClient==null)
-                return null;
+            if(localDelegate==null)
+                localDelegate = new defaultDelegate();
             try {
                 instance = new Connector(new URI(localServer));
             } catch (URISyntaxException e) {
@@ -44,8 +61,8 @@ public final class Connector extends WebSocketClient{
         localSender = sender;
     }
 
-    public static void setClient(String client) {
-        localClient = client;
+    public static void setEndpoint(String endpoint) {
+        localEndpoint = endpoint;
     }
 
     public static void destroy() {
@@ -72,15 +89,26 @@ public final class Connector extends WebSocketClient{
         localDelegate.onError(ex);
     }
 
+    /**
+     * Send login request in JSON format.
+     * @param  password                 user's password in md5Hex
+     * @throws NotYetConnectedException not connected.
+     */
     public void login(String password) throws NotYetConnectedException {
+        java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         JSONObject msg = new JSONObject();
-        JSONObject event = new JSONObject();
-        msg.put("account", localSender);
-        msg.put("password", password);
-        event.put("event", "login");
-        event.put("client", localClient);
-        event.put("content", msg);
-        super.send(event.toString());
+        JSONObject content = new JSONObject();
+
+        msg.put("event", "login");
+        msg.put("endpoint", localEndpoint);
+
+        content.put("account", localSender);
+        content.put("password", password);
+        content.put("time", df.format(new java.util.Date()));
+
+        msg.put("content", content);
+
+        super.send(msg.toString());
     }
 
     public void talk(String receiver, String text) throws NotYetConnectedException {
@@ -90,7 +118,7 @@ public final class Connector extends WebSocketClient{
         msg.put("receiver", receiver);
         msg.put("text", text);
         event.put("event", "talk");
-        event.put("client", localClient);
+        event.put("client", localEndpoint);
         event.put("content", msg);
         super.send(event.toString());
     }
