6436f40c02045b367024ec65bcb392b20073cc0b
Ciel <imwithye@gmail.com>
2014-02-26 22:21:48 +0800

    Add better logging in LoginView.

--- examinee/src/main/java/studymaster/examinee/ViewController/LoginView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/LoginView.java
@@ -11,42 +11,56 @@ public class LoginView extends LoginViewController {
 
 	@Override
 	public void onOpen(short httpStatus, String httpStatusMessage) {
-		System.out.println("Socket's connection established.");
+		System.out.println("[info] (LoginView onOpen) Socket's connection established.");
 	}
 
 	@Override
 	public void onClose(int code, String reason, boolean remote) {
-
+		System.out.println("[info] (LoginView onClose) Socket's connection closed.");
 	}
 
 	@Override
 	public void onMessage(String message) {
-		System.out.println("Message: " + message);
-		JSONObject event = new JSONObject(message);
+		System.out.println("[info] (LoginView onMessage) Receive message: " + message);
+		try {
+			JSONObject msg = new JSONObject(message);
+        	String event = msg.getString("event");
+        	String endpoint = msg.getString("endpoint");
+        	JSONObject content = msg.getJSONObject("content");
 
-		if(event.getString("event").equals("login")) {
-			JSONObject content = event.getJSONObject("content");
-			if(content.getString("status").equals("success")) {
-				javafx.application.Platform.runLater(new Runnable() {
-  					@Override
-  					public void run() {
-  						try {
-  							director.pushStageWithFXML(App.class.getResource("/fxml/home.fxml"));
-  						} catch (Exception e) {
-  							System.out.println(e);
-  						}	
-  					}
-				});
+			if(event.equals("login")) {
+				String status = content.getString("status");
+
+				if(status.equals("success")) {
+					System.out.println("[info] (LoginView onMessage) Login successfully.");
+					javafx.application.Platform.runLater(new Runnable() {
+  						@Override
+  						public void run() {
+  							try {
+  								director.pushStageWithFXML(App.class.getResource("/fxml/home.fxml"));
+  							} catch (Exception e) {
+  								System.err.println("[err] (LoginView onMessage) Error when switching scene");
+  							}
+  						}
+					});
+				}
+
+				else if(status.equals("failed")) {
+					System.out.println("[info] (LoginView onMessage) Login failed.");
+				}
+
+				else {
+					System.err.println("[err] (LoginView onMessage) Unexpected JSON response string.");
+				}
 			}
-			else {
-				//do something if login faild.
-			}
+		} catch (Exception e) {
+			System.err.println("[err] (LoginView onMessage) Error when decoding JSON response string.");
 		}
 	}
 
 	@Override
 	public void onError(Exception ex) {
-
+		System.err.println("[err] (LoginView onError) An error has been caught.");
 	}
 
 	@Override
@@ -56,7 +70,7 @@ public class LoginView extends LoginViewController {
 			connector.login(password);
 		}
 		else {
-			System.err.println("Waiting for connection.");
+			System.err.println("[info] (LoginView login) No connection. Waiting for connection.");
 		}
 	}
 }
