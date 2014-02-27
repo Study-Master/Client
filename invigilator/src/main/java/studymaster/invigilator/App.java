bd4897749224057de08d6025d8caee5643e7b46b
Samyro <zh0021in@e.ntu.edu.sg>
2014-02-27 19:08:08 +0800

    Invigilator login page

---
+++ invigilator/src/main/java/studymaster/invigilator/App.java
@@ -0,0 +1,24 @@
+package studymaster.invigilator;
+
+import studymaster.socket.Connector;
+import studymaster.all.Model.Client;
+import studymaster.all.ViewController.Director;
+
+public class App extends Client {
+
+	public static void main(String[] args) throws Exception {
+		//Configure
+		Connector.setServer(Configure.SERVER);
+		Connector.setEndpoint(Configure.ENDPOINT);
+
+		//launch
+		launch(args);
+	}
+
+	@Override
+	public void start(javafx.stage.Stage stage) throws Exception {
+		stage.setTitle("Study Master");
+		director.setStage(stage);
+        director.pushStageWithFXML(App.class.getResource("/fxml/loginView.fxml"));
+    }
+}
---
+++ invigilator/src/main/java/studymaster/invigilator/Configure.java
@@ -0,0 +1,6 @@
+package studymaster.invigilator;
+
+public class Configure {
+	public static final String SERVER = "ws://localhost:8088";
+	public static final String ENDPOINT = "Java Invigilator Client";
+}
