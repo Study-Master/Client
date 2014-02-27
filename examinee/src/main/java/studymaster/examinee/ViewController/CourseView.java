82e042b5ebe98d94e051c20723c04e576239e9df
Ciel <imwithye@gmail.com>
2014-02-27 23:48:54 +0800

    Implement add component method!!!!

--- examinee/src/main/java/studymaster/examinee/ViewController/CourseView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/CourseView.java
@@ -5,6 +5,7 @@ import studymaster.all.ViewController.Director;
 import studymaster.socket.Connector;
 import studymaster.examinee.App;
 import org.json.JSONObject;
+import javafx.scene.layout.AnchorPane;
 
 public class CourseView extends HomeViewController {
 
@@ -17,7 +18,18 @@ public class CourseView extends HomeViewController {
         	String endpoint = msg.getString("endpoint");
         	JSONObject content = msg.getJSONObject("content");
 
-        	//add code here
+			final AnchorPane pane = (AnchorPane) director.getScene().getRoot();
+        	
+        	javafx.application.Platform.runLater(new Runnable() {
+  				@Override
+  				public void run() {
+  					try {
+  						//add code here
+  					} catch (Exception e) {
+  						System.err.println("[err] (CourseView onMessage) Error when adding component.");
+  					}
+  				}
+			});
 		} catch (Exception e) {
 			System.err.println("[err] (CourseView onMessage) Error when decoding JSON response string.");
 		}
