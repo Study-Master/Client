7b787483578fdd7d65927bcf7c3f76e0dade498e
cxxkxxm <xc540863058@gmail.com>
2014-02-27 22:10:14 +0800

    aaa
    aaa

--- examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
@@ -1,7 +1,17 @@
-package studymaster.invigilator.ViewController;
+package studymaster.examinee.ViewController;
 
 import studymaster.all.ViewController.TestViewController;
 import studymaster.all.ViewController.Director;
 import studymaster.socket.Connector;
-import studymaster.invigilator.App;
+import studymaster.examinee.App;
 import org.json.JSONObject;
+
+public class LoginView extends LoginViewController {
+
+	@Override
+	public void onMessage(String message) {
+        System.out.println("[info] (LoginView onMessage) Receive message: " + message);
+
+	}
+
+}
