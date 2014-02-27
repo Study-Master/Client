c512086b1d68be711530114e208728f3cc0d8571
cxxkxxm <xc540863058@gmail.com>
2014-02-27 21:38:04 +0800

    123
    1333

--- all/src/main/java/studymaster/all/ViewController/TestViewController.java
+++ all/src/main/java/studymaster/all/ViewController/TestViewController.java
@@ -2,3 +2,10 @@ package studymaster.all.ViewController;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+//import javafx.scene.control.TestField;
+//import javafx.scene.control.NameField;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public abstract class TestViewController extends ViewController {
+    //@FXML
+}
---
+++ examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
@@ -0,0 +1,7 @@
+package studymaster.invigilator.ViewController;
+
+import studymaster.all.ViewController.TestViewController;
+import studymaster.all.ViewController.Director;
+import studymaster.socket.Connector;
+import studymaster.invigilator.App;
+import org.json.JSONObject;
