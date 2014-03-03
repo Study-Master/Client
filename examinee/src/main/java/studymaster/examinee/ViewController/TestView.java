8ec02652443b79a9fb0f0e6b2dd01024729e4333
Ciel <imwithye@gmail.com>
2014-03-03 20:52:02 +0800

    Refactor TestView.java

--- examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
@@ -14,25 +14,13 @@ public class TestView extends TestViewController {
     
 	@Override
 	public void onMessage(String message) {
-		System.out.println("[info] (TestView onMessage) Receive message: " + message);
-
-		try {
-                    System.out.println("[info] (TestView onMessage) Test test");
-
-		} catch (Exception e) {
-			System.err.println("[err] (TestView onMessage)");
-		}
+		System.out.println("[info] ("+ getClass().getSimpleName() +" onMessage) Receive message: " + message);
 	}
    
-
 	@Override
 	public void test() {
-            String test1 = "success";
-            try {
-                if (test1.equals("success"))
-                    director.pushStageWithFXML(getClass().getResource("/fxml/testView2.fxml"));
-            } catch (Exception e) {
-                System.err.println("[err] (testView) Error when enter testView stage");
-            }
+		String test1 = "success";
+		if (test1.equals("success"))
+			director.pushStageWithFXML(getClass().getResource("/fxml/testView2.fxml"));
 	}
 }
