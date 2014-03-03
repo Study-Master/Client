bcab2ede4b6eb9020a83c576d5aa3c3fe8325721
Ciel <imwithye@gmail.com>
2014-03-04 00:10:02 +0800

    Make CourseView better:(

--- examinee/src/main/java/studymaster/examinee/ViewController/CourseView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/CourseView.java
@@ -28,11 +28,11 @@ import javafx.util.Duration;
 
 public class CourseView extends HomeViewController {
 
-	@Override
+  @Override
 	public void onMessage(String message) {
-		System.out.println("[info] ("+ getClass().getSimpleName() +" onMessage) Receive message: " + message);
-		try {
-			JSONObject msg = new JSONObject(message);
+    System.out.println("[info] ("+ getClass().getSimpleName() +" onMessage) Receive message: " + message);
+    try {
+      JSONObject msg = new JSONObject(message);
       String event = msg.getString("event");
       String endpoint = msg.getString("endpoint");
       final JSONObject content = msg.getJSONObject("content");
@@ -40,45 +40,45 @@ public class CourseView extends HomeViewController {
       if(event.equals("profile")) {
         showCourseList(content);
       }
-
     } catch (Exception e) {
-     System.err.println("[err] ("+ getClass().getSimpleName() +" onMessage) Error when decoding JSON response string.");
-   }
- }
+      System.err.println("[err] ("+ getClass().getSimpleName() +" onMessage) Error when decoding JSON response string.");
+    }
+  }
 
- private void showCourseList(final JSONObject content) {
-  final AnchorPane pane = (AnchorPane) director.getScene().getRoot();
-  javafx.application.Platform.runLater(new Runnable() {
-    @Override
-    public void run() {
-      try {
-        JSONObject profile = content.getJSONObject("profile");
-        JSONArray courses = profile.getJSONArray("courses");
-        String status;
-        GridPane courseList = new GridPane();
-        AnchorPane.setTopAnchor(courseList, 150.0);
-        AnchorPane.setLeftAnchor(courseList, 90.0);
-        AnchorPane.setRightAnchor(courseList, 90.0);
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(15);
-        ColumnConstraints col2 = new ColumnConstraints();
-        col2.setPercentWidth(65);
-        ColumnConstraints col3 = new ColumnConstraints();
-        col3.setPercentWidth(20);
-        courseList.getColumnConstraints().addAll(col1,col2,col3);
-          courseList.setStyle("-fx-border: 2px solid; -fx-border-color: red; -fx-border-insets: 5;"); //debug only
+  private void showCourseList(final JSONObject content) {
+    final AnchorPane pane = (AnchorPane) director.getScene().getRoot();
+    javafx.application.Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          JSONObject profile = content.getJSONObject("profile");
+          JSONArray courses = profile.getJSONArray("courses");
+          String status;
+          GridPane courseList = new GridPane();
+          AnchorPane.setTopAnchor(courseList, 150.0);
+          AnchorPane.setLeftAnchor(courseList, 90.0);
+          AnchorPane.setRightAnchor(courseList, 90.0);
+          ColumnConstraints col1 = new ColumnConstraints();
+          col1.setPercentWidth(15);
+          ColumnConstraints col2 = new ColumnConstraints();
+          col2.setPercentWidth(65);
+          ColumnConstraints col3 = new ColumnConstraints();
+          col3.setPercentWidth(20);
+          courseList.getColumnConstraints().addAll(col1,col2,col3);
+          courseList.setStyle("-fx-border: 2px solid; -fx-border-color: red; -fx-border-insets: 5;");
 
           for(int i=0; i<courses.length(); i++) {
             JSONObject course = courses.getJSONObject(i);
+
             Label code = new Label(course.getString("code"));
             Label name = new Label(course.getString("name"));
             courseList.add(code, 0, i);
             courseList.add(name, 1, i);
 
             status = course.getString("status");
+
             if (status.equals("unbooked")) {
               Button button = new Button(" Book ");
-
               button.setOnAction(new EventHandler<ActionEvent>() {
                 @Override public void handle(ActionEvent e) {
                   director.pushStageWithFXML(getClass().getResource("/fxml/bookingView.fxml"));
@@ -87,7 +87,8 @@ public class CourseView extends HomeViewController {
 
               courseList.add(button, 2, i);
             }
-            else if (status.equals("booked")) {      
+
+            else if (status.equals("booked")) {
               try {
                 String examStartTime = course.getString("start_time");
                 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -95,21 +96,18 @@ public class CourseView extends HomeViewController {
                 Date startTime = dateFormat.parse(examStartTime);
                 long diff = startTime.getTime() - currentTime.getTime();
                 long diffDays = diff / (24 * 60 * 60 * 1000);
-                //System.out.println(diffDays);
-                if (diffDays<3){
+
+                if (diffDays<3) {
                   CountDown timeLabel = new CountDown(examStartTime);
                   courseList.add(timeLabel, 2, i);
                 }
                 else {
                   Button button = new Button("Cancel");
-                  courseList.add(button, 2, i);            
+                  courseList.add(button, 2, i);
                 }
+              } catch (Exception e){
+                System.err.println("[err] ("+ getClass().getSimpleName() +")Error when parsing string");
               }
-              catch (Exception e){
-                System.err.println("[err] Error when parsing string");
-              }
-              
-              
             }
           }
           pane.getChildren().addAll(courseList);
@@ -118,9 +116,7 @@ public class CourseView extends HomeViewController {
         }
       }
     });
-}
-
-
+  }
 }
 
 class CountDown extends Label {
@@ -128,32 +124,31 @@ class CountDown extends Label {
     bindToTime(remainingTime);
   }
 
-  // the digital clock updates once a second.
   private void bindToTime(final String examStartTime) {
     Timeline timeline = new Timeline(
       new KeyFrame(Duration.seconds(0),
         new EventHandler<ActionEvent>() {
-          @Override 
+          @Override
           public void handle(ActionEvent actionEvent) {
-            try {              
+            try {
               setText(getRemainingTime(examStartTime));
             } catch (ParseException ex) {
               Logger.getLogger(CountDown.class.getName()).log(Level.SEVERE, null, ex);
             }
           }
         }
-        ),
+      ),
       new KeyFrame(Duration.seconds(1))
-      );
+    );
     timeline.setCycleCount(Animation.INDEFINITE);
     timeline.play();
   }
-  
+
   public static String getRemainingTime(String examStartTime) throws ParseException {
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     Date currentTime = new Date();
     Date startTime = dateFormat.parse(examStartTime);
-    
+
     long diff = startTime.getTime() - currentTime.getTime();
     long diffSeconds = diff / 1000 % 60;
     long diffMinutes = diff / (60 * 1000) % 60;
@@ -163,8 +158,7 @@ class CountDown extends Label {
       return diffDays + "D " + diffHours + "H " + diffMinutes + "M" ;
     }
     else {
-      return diffHours + "H " + diffMinutes + "M " + diffSeconds + "S" ;       
+      return diffHours + "H " + diffMinutes + "M " + diffSeconds + "S" ;
     }
   }
-  
 }
