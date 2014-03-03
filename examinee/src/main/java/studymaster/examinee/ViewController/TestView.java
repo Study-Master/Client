f484fa2b67805b81da06f32a552c0c348140221d
Ciel <imwithye@gmail.com>
2014-03-03 23:42:25 +0800

    Revert 0867250..2e18613
    This rolls back to commit 086725063f9b511261b0cf56d7a7052f25f7f7e4.

---
+++ examinee/src/main/java/studymaster/examinee/ViewController/TestView.java
@@ -0,0 +1,26 @@
+package studymaster.examinee.ViewController;
+
+import studymaster.all.ViewController.TestViewController;
+import studymaster.all.ViewController.Director;
+import studymaster.socket.Connector;
+import studymaster.examinee.App;
+import org.json.JSONObject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TestView extends TestViewController {
+
+    
+	@Override
+	public void onMessage(String message) {
+		System.out.println("[info] ("+ getClass().getSimpleName() +" onMessage) Receive message: " + message);
+	}
+   
+	@Override
+	public void test() {
+		String test1 = "success";
+		if (test1.equals("success"))
+			director.pushStageWithFXML(getClass().getResource("/fxml/testView2.fxml"));
+	}
+}
