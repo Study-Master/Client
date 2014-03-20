64b44cbf736c49d36de838ce9650e3b407380557
Ciel <imwithye@gmail.com>
2014-03-20 15:20:38 +0800

    Add chat windows

--- invigilator/src/main/java/studymaster/invigilator/App.java
+++ invigilator/src/main/java/studymaster/invigilator/App.java
@@ -20,6 +20,6 @@ public class App extends Client {
 	public void start(javafx.stage.Stage stage) throws Exception {
 		stage.setTitle("Study Master");
 		director.setStage(stage);
-        director.pushStageWithFXML(App.class.getResource("/fxml/authView.fxml"));
+        director.pushStageWithFXML(App.class.getResource("/fxml/invigilatorView.fxml"));
     }
 }
