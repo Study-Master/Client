d446f75c00b24351c404e47bcf9ce94622daf904
Samyro <zh0021in@e.ntu.edu.sg>
2014-03-20 21:58:33 +0800

    To merge

--- examinee/src/main/java/studymaster/examinee/Configure.java
+++
@@ -1,6 +0,0 @@
-package studymaster.examinee;
-
-public class Configure {
-	public static final String SERVER = "ws://cz2006.ciel.im:8087";
-	public static final String ENDPOINT = "Java Examinee Client";
-}
