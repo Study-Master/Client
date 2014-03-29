aa9ef7e063c05f362a19be268511f937b5b3b314
Ciel <imwithye@gmail.com>
2014-03-29 13:04:44 +0800

    add streaming function to webcam

--- media/src/main/java/studymaster/media/Webcamera.java
+++ media/src/main/java/studymaster/media/Webcamera.java
@@ -1,17 +1,19 @@
 package studymaster.media;
 
 import com.github.sarxos.webcam.Webcam;
+import java.awt.image.BufferedImage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import studymaster.socket.VideoCl;
 
 public class Webcamera {
-	private static Webcamera instance;
-	private static Webcam webcam;
+    private static Webcamera instance;
+    private static Webcam webcam;
     private static boolean isRunning;
-	public static volatile boolean isStreaming;
+    public static volatile boolean isStreaming;
     public static volatile boolean isOpening;
     public static volatile boolean isClosing;
-	private static class WebcamThread extends Thread {
+    private static class WebcamThread extends Thread {
         private ImageView view;
 
 
@@ -26,28 +28,55 @@ public class Webcamera {
         @Override public void run() {
             while (isClosing);
             while (isStreaming) {
-                Image image = createImage();
+                Image image = ImgUtil.createImage(createImage());
                 view.setImage(image);
                 isOpening = false;
             }
         }
     }
+    private static class WebcamStreamThread extends Thread {
+        private ImageView view;
+        private VideoCl videoCl;
+
+        public WebcamStreamThread(ImageView view) {
+            videoCl = VideoCl.getInstance();
+            this.view = view;
+        }
+
+        public void setImageView(ImageView view) {
+            this.view = view;
+        }
+
+        @Override public void run() {
+            while (isClosing);
+            while (isStreaming) {
+                BufferedImage bufferedImage = createImage();
+                Image image = ImgUtil.createImage(bufferedImage);
+                byte[] byteImage = ImgUtil.toByte(bufferedImage);
+                videoCl.send(byteImage);
+                view.setImage(image);
+                
+                isOpening = false;
+            }
+        }
+    }
     private static class WebcamCloseThread extends Thread {
-    	@Override public void run() {
-    		while (isOpening);
-    		if (!isStreaming) {
-    			if(webcam.isOpen() && webcam!=null) {
+        @Override public void run() {
+            while (isOpening);
+            if (!isStreaming) {
+                if(webcam.isOpen() && webcam!=null) {
                     isClosing = true;
-    				webcam.close();
+                    webcam.close();
                     isClosing = false;
-    			}
-    		}
-    	}
+                }
+            }
+        }
     }
     private static WebcamThread wt;
+    private static WebcamStreamThread wst;
     private static WebcamCloseThread wct;
 
-	private Webcamera() {
+    private Webcamera() {
         isRunning = false;
         isStreaming = false;
         isOpening = false;
@@ -55,39 +84,39 @@ public class Webcamera {
         wt = new WebcamThread(null);
     }
 
-	public static Webcamera getInstance() {
-		if (instance == null) {
-			instance = new Webcamera();
-		}
-		else {}
-		return instance;
-	}
+    public static Webcamera getInstance() {
+        if (instance == null) {
+            instance = new Webcamera();
+        }
+        else {}
+        return instance;
+    }
 
-	public static Image createImage() {
-		if(webcam == null) {
+    public static BufferedImage createImage() {
+        if(webcam == null) {
             isOpening = true;
-			webcam = Webcam.getDefault();
-		}
-		else {}
+            webcam = Webcam.getDefault();
+        }
+        else {}
 
-		if(!webcam.isOpen()) {
+        if(!webcam.isOpen()) {
             isOpening = true;
-			webcam.open(false);
-		}
-		else {}
-		return ImgUtil.createImage(webcam.getImage());
-	}
+            webcam.open(false);
+        }
+        else {}
+        return webcam.getImage();
+    }
 
-	public static void stop() {
+    public static void stop() {
         if (isStreaming) {
             isStreaming = false;
             wct = new WebcamCloseThread();
             wct.start();
         }
-		else {}
-	}
+        else {}
+    }
 
-	public void start(ImageView imageView) {
+    public void start(ImageView imageView) {
         if (isStreaming) {
             wt.setImageView(imageView);
         } else {
@@ -95,5 +124,14 @@ public class Webcamera {
             wt = new WebcamThread(imageView);
             wt.start();
         }
-	}
+    }
+
+    public void startStreaming(ImageView imageView) {
+       if (isStreaming) {}
+       else {
+           isStreaming = true;
+           wst = new WebcamStreamThread(imageView);
+           wst.start();
+       }
+    }
 }
--- socket/src/main/java/studymaster/socket/VideoCl.java
+++ socket/src/main/java/studymaster/socket/VideoCl.java
@@ -12,56 +12,56 @@ import org.java_websocket.handshake.ServerHandshake;
 import java.nio.channels.NotYetConnectedException;
 
 public class VideoCl extends WebSocketClient {
-	private static VideoCl instance = null;
-	private static String localServer = null;
-	private static String localSender = "Default Sender";
-	private static String localEndpoint = "Default VideoCl";
-	private static ImageView imgView = null;
-	private static Webcam webcam;
+    private static VideoCl instance = null;
+    private static String localServer = null;
+    private static String localSender = "Default Sender";
+    private static String localEndpoint = "Default VideoCl";
+    private static ImageView imgView = null;
+    private static Webcam webcam;
 
-	private VideoCl(URI serverURI) {
-        	super(serverURI);
-	}
+    private VideoCl(URI serverURI) {
+            super(serverURI);
+    }
 
-	public static VideoCl getInstance() {
-		if(localServer == null && imgView == null)
-			return null;
-		else {
-			if(instance == null) {
-				try {
-					instance = new VideoCl(new URI(localServer));
-				} catch(Exception e) {
-					instance = null;
-				}
-				return instance;
-			}
-			return instance;
-		}
-	}
+    public static VideoCl getInstance() {
+        if(localServer == null && imgView == null)
+            return null;
+        else {
+            if(instance == null) {
+                try {
+                    instance = new VideoCl(new URI(localServer));
+                } catch(Exception e) {
+                    instance = null;
+                }
+                return instance;
+            }
+            return instance;
+        }
+    }
 
-	public static void setServer(String server) {
-		localServer = server;
-	}
+    public static void setServer(String server) {
+        localServer = server;
+    }
 
-	public static void setSender(String sender) {
-		localSender = sender;
-	}
+    public static void setSender(String sender) {
+        localSender = sender;
+    }
 
-	public static void setEndpoint(String endpoint) {
-		localEndpoint = endpoint;
-	}
+    public static void setEndpoint(String endpoint) {
+        localEndpoint = endpoint;
+    }
 
-	public static void setImageView(ImageView imageView) {
-		imgView = imageView;
-	}
+    public static void setImageView(ImageView imageView) {
+        imgView = imageView;
+    }
 
-	public static void updateImageView(Image image) {
-		imgView.setImage(image);
-	}
+    public static void updateImageView(Image image) {
+        imgView.setImage(image);
+    }
 
-	@Override
+    @Override
     public void onOpen(ServerHandshake handshakedata) {
-    	request();
+        request();
     }
 
     @Override
@@ -69,59 +69,59 @@ public class VideoCl extends WebSocketClient {
 
     @Override
     public void onMessage(String message) {
-    	System.out.println("[info] (VideoCl onMessage) Receive message: " + message);
-		JSONObject msg = new JSONObject(message);
+        System.out.println("[info] (VideoCl onMessage) Receive message: " + message);
+        JSONObject msg = new JSONObject(message);
 
-		String event = msg.getString("event");
-		String endpoint = msg.getString("endpoint");
-		JSONObject content = msg.getJSONObject("content");
+        String event = msg.getString("event");
+        String endpoint = msg.getString("endpoint");
+        JSONObject content = msg.getJSONObject("content");
 
-		if(event.equals("register")) {
-			String status = content.getString("status");
-			if(status.equals("success"))
-				videoStart();
-		}
-	}
+        if(event.equals("register")) {
+            String status = content.getString("status");
+            if(status.equals("success"))
+                videoStart();
+        }
+    }
 
-	@Override
+    @Override
     public void onError(Exception ex) {}
 
     private void request() {
-    	JSONObject msg = new JSONObject();
-    	JSONObject content = new JSONObject();
-    	msg.put("event", "register");
-    	msg.put("endpoint", localEndpoint);
-    	msg.put("content", content);
-		super.send(msg.toString());
-	}
+        JSONObject msg = new JSONObject();
+        JSONObject content = new JSONObject();
+        msg.put("event", "register");
+        msg.put("endpoint", localEndpoint);
+        msg.put("content", content);
+        super.send(msg.toString());
+    }
 
-	private BufferedImage processImage() {
-		if(webcam==null)
-			webcam = Webcam.getDefault();
-		if(!webcam.isOpen())
-			webcam.open();
-		return webcam.getImage();
-	}
+    private BufferedImage processImage() {
+        if(webcam==null)
+            webcam = Webcam.getDefault();
+        if(!webcam.isOpen())
+            webcam.open();
+        return webcam.getImage();
+    }
 
-	public void videoStart() {
-		Thread videoStream = new Thread() {
-			@Override
-			public void run(){
-				while(true) {
-					VideoCl videoClient = VideoCl.getInstance();
-					try {
-						BufferedImage bufferedImage = videoClient.processImage();
-						videoClient.updateImageView(ImgUtil.createImage(bufferedImage));
+    public void videoStart() {
+        Thread videoStream = new Thread() {
+            @Override
+            public void run(){
+                while(true) {
+                    VideoCl videoClient = VideoCl.getInstance();
+                    try {
+                        BufferedImage bufferedImage = videoClient.processImage();
+                        videoClient.updateImageView(ImgUtil.createImage(bufferedImage));
 
-						byte[] byteImage = ImgUtil.toByte(bufferedImage);
-						videoClient.send(byteImage);
-						Thread.sleep(200);
-					} catch(InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
-		videoStream.run();
-	}
+                        byte[] byteImage = ImgUtil.toByte(bufferedImage);
+                        videoClient.send(byteImage);
+                        Thread.sleep(200);
+                    } catch(InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        videoStream.run();
+    }
 }
