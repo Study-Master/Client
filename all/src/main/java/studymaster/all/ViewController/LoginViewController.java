19f2f9aea554fd03a7de68895c277fe77fc8fa28
Ciel <imwithye@gmail.com>
2014-04-02 18:20:22 +0800

    New login loading design

--- all/src/main/java/studymaster/all/ViewController/LoginViewController.java
+++ all/src/main/java/studymaster/all/ViewController/LoginViewController.java
@@ -5,7 +5,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -14,14 +13,14 @@ public abstract class LoginViewController extends ViewController{
     @FXML protected TextField accountField;
     @FXML protected PasswordField passwordField;
     @FXML protected Button loginButton;
+    @FXML protected Button exitButton;
+    @FXML protected ImageView loadingImgView;
 
     @FXML public final void loginAction() {
         System.out.println("[info] (" + getClass().getSimpleName() + " loginAction) Trying to login...");
         String account = accountField.getText();
         String password = DigestUtils.md5Hex(passwordField.getText());
-
         loginStart();
-
         login(account, password);
     }
 
@@ -33,24 +32,28 @@ public abstract class LoginViewController extends ViewController{
     protected abstract void login(String account, String password);
 
     protected void loginStart() {
-        loginButton.setText("");
-        Image LoadingIcon = new Image(getClass().getResourceAsStream("/image/Loading.gif"));
-        loginButton.setGraphic(new ImageView(LoadingIcon));
-        loginButton.setStyle("-fx-padding-left: 0; -fx-background-color: rgba(0, 102, 153, 1);");
+        loadingImgView.setVisible(true);
+        accountField.setDisable(true);
+        passwordField.setDisable(true);
         loginButton.setDisable(true);
+        loginButton.setVisible(false);
+        exitButton.setVisible(false);
     }
 
-    protected void loginFailed() {
+    protected void loginFailed(String reason) {
         AlertAction action = new AlertAction() {
             @Override public void ok(Stage stage) {
                 connector.renew();
                 stage.close();
+                loadingImgView.setVisible(false);
                 loginButton.setDisable(false);
-                loginButton.setText("Login");
-                loginButton.setGraphic(null);
-                loginButton.setStyle("-fx-background-color: white;");
+                accountField.setDisable(false);
+                passwordField.setDisable(false);
+                passwordField.clear();
+                loginButton.setVisible(true);
+                exitButton.setVisible(true);
             }
         };
-        director.invokeErrorAlert("Error, please check your Internet connection or restart the program.", action);
+        director.invokeErrorAlert(reason, action);
     }
 }
