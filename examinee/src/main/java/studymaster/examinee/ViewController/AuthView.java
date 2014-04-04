c9cb4eabfc6505cac7e5e9a44ac1816a5921343e
Wang Qilin <wangqilin129@gmail.com>
2014-04-04 22:52:18 +0800

    根本停不下来！

--- examinee/src/main/java/studymaster/examinee/ViewController/AuthView.java
+++ examinee/src/main/java/studymaster/examinee/ViewController/AuthView.java
@@ -8,21 +8,44 @@ import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import studymaster.examinee.Configure;
+import org.json.JSONObject;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
 
 public class AuthView extends ViewController implements VideoEventHandler {
 
     @FXML private ImageView imgView;
     @FXML private Button startButton;
+    @FXML private Label label1;
+    @FXML private Label label2;
     private VideoCl videoCl;
 
     @Override public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
         super.initialize(location, resources);
         videoCl = VideoCl.getInstance(this);
-        startButton.setDisable(true);
+        startButton.setVisible(false);
+        label1.setText("Please keep facing the webcam.");
+        label2.setText("Authentication takes about one minute.");
         videoCl.connect();
     }
     
-    @Override public void onMessage(String message) {}
+    @Override public void onMessage(String message) {
+        JSONObject msg = new JSONObject(message);
+        String event = msg.getString("event");
+        if (event.equals("auth_successful")) {
+            label1.setText("");
+            label2.setText("You may click the \"Start\" button to start exam now.");
+            startButton.setVisible(true);    
+            startButton.setDisable(false);
+            startButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    director.pushStageWithFXML(getClass().getResource("/fxml/examView.fxml"));
+                }
+            });
+        }
+    }
 
     @Override public void onVideoClientOpen() {
         System.out.println("[info] (" + getClass().getSimpleName() + " onVideoClientOpen)");
